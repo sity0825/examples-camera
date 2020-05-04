37fb6f1dd6a583a26ef2b55c5806da7ab61cc245
sity0825 <tatsuya.shimoji@g.softbank.co.jp>
2020-05-04 16:42:03 +0900

    first commit

---
+++ opencv/person_count_rev0.2.py
@@ -0,0 +1,174 @@
+
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""A demo that runs object detection on camera frames using OpenCV.
+
+TEST_DATA=../all_models
+
+Run face detection model:
+python3 detect.py \
+  --model ${TEST_DATA}/mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite
+
+Run coco model:
+python3 detect.py \
+  --model ${TEST_DATA}/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite \
+  --labels ${TEST_DATA}/coco_labels.txt
+
+"""
+import argparse
+import collections
+import common
+import cv2
+import numpy as np
+import os
+from PIL import Image
+import re
+import tflite_runtime.interpreter as tflite
+
+Object = collections.namedtuple('Object', ['id', 'score', 'bbox'])
+
+def load_labels(path):
+    p = re.compile(r'\s*(\d+)(.+)')
+    with open(path, 'r', encoding='utf-8') as f:
+       lines = (p.match(line).groups() for line in f.readlines())
+       return {int(num): text.strip() for num, text in lines}
+
+class BBox(collections.namedtuple('BBox', ['xmin', 'ymin', 'xmax', 'ymax'])):
+    """Bounding box.
+    Represents a rectangle which sides are either vertical or horizontal, parallel
+    to the x or y axis.
+    """
+    __slots__ = ()
+
+def get_output(interpreter, score_threshold, top_k, image_scale=1.0):
+    """Returns list of detected objects."""
+    boxes = common.output_tensor(interpreter, 0)
+    class_ids = common.output_tensor(interpreter, 1)
+    scores = common.output_tensor(interpreter, 2)
+    count = int(common.output_tensor(interpreter, 3))
+
+    def make(i):
+        ymin, xmin, ymax, xmax = boxes[i]
+        return Object(
+            id=int(class_ids[i]),
+            score=scores[i],
+            bbox=BBox(xmin=np.maximum(0.0, xmin),
+                      ymin=np.maximum(0.0, ymin),
+                      xmax=np.minimum(1.0, xmax),
+                      ymax=np.minimum(1.0, ymax)))
+
+    return [make(i) for i in range(top_k) if scores[i] >= score_threshold]
+
+def main():
+    default_model_dir = '../all_models'
+    default_model = 'mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite'
+    #default_model = 'mobilenet_ssd_v2_face_quant_postprocess_edgetpu.tflite'
+    #default_model = 'mobilenet_v2_1.0_224_quant_edgetpu.tflite'
+    default_labels = 'coco_labels.txt'
+    #default_labels = 'imagenet_labels.txt'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', help='.tflite model path',
+                        default=os.path.join(default_model_dir,default_model))
+    parser.add_argument('--labels', help='label file path',
+                        default=os.path.join(default_model_dir, default_labels))
+    parser.add_argument('--top_k', type=int, default=10,
+                        help='number of categories with highest score to display')
+    parser.add_argument('--camera_idx', type=int, help='Index of which video source to use. ', default = 0)
+    parser.add_argument('--threshold', type=float, default=0.3,
+                        help='classifier score threshold')
+    args = parser.parse_args()
+
+    print('Loading {} with {} labels.'.format(args.model, args.labels))
+    interpreter = common.make_interpreter(args.model)
+    #print(interpreter)
+    interpreter.allocate_tensors()
+    labels = load_labels(args.labels)
+
+    #cap = cv2.VideoCapture(args.camera_idx)
+    filepath = "/home/pi/testPJ/vtest.avi"
+    cap = cv2.VideoCapture(filepath)
+    
+    #print(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    #print(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))    
+    center_widht = cap.get(cv2.CAP_PROP_FRAME_WIDTH) / 2
+    center_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT) / 2
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+        cv2_im = frame
+
+        cv2_im_rgb = cv2.cvtColor(cv2_im, cv2.COLOR_BGR2RGB)
+        pil_im = Image.fromarray(cv2_im_rgb)
+
+        common.set_input(interpreter, pil_im)
+        interpreter.invoke()
+        objs = get_output(interpreter, score_threshold=args.threshold, top_k=args.top_k)
+        cv2_im = append_objs_to_img(cv2_im, objs, labels, center_widht, center_height)
+        drawing_line(cv2_im, center_widht, center_height)
+
+        cv2.imshow('frame', cv2_im)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+def append_objs_to_img(cv2_im, objs, labels, center_widht, center_height):
+    top_left_count = 0
+    top_right_count = 0
+    bottom_left_count = 0
+    bottom_right_count = 0
+    
+    height, width, channels = cv2_im.shape
+    
+    for obj in objs:
+        #print(obj)
+        if labels.get(obj.id, obj.id) == "person":
+            x0, y0, x1, y1 = list(obj.bbox)
+            x0, y0, x1, y1 = int(x0*width), int(y0*height), int(x1*width), int(y1*height)
+            percent = int(100 * obj.score)
+            label = '{}%'.format(percent)
+            #print(labels.get(obj.id, obj.id))
+            cv2_im = cv2.rectangle(cv2_im, (x0, y0), (x1, y1), (0, 255, 0), 2)
+            cv2_im = cv2.putText(cv2_im, label, (x0, y0),
+                                 cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 0), 2)
+            if center_height >= (y0+y1)/2 and center_widht >= (x0+x1)/2:               
+                top_left_count+=1
+            if center_height >= (y0+y1)/2 and center_widht < (x0+x1)/2:
+                top_right_count+=1
+            if center_height < (y0+y1)/2 and center_widht >= (x0+x1)/2:
+                bottom_left_count+=1
+            if center_height < (y0+y1)/2 and center_widht < (x0+x1)/2:
+                bottom_right_count+=1
+
+
+    #print('person_count:{}'.format(count))
+    cv2_im = cv2.putText(cv2_im, 'count: {}'.format(top_left_count),(10,30),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    cv2_im = cv2.putText(cv2_im, 'count: {}'.format(top_right_count),(int(center_widht)+10,30),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    cv2_im = cv2.putText(cv2_im, 'count: {}'.format(bottom_left_count),(10,int(center_height)+30),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+    cv2_im = cv2.putText(cv2_im, 'count: {}'.format(bottom_right_count),(int(center_widht)+10,int(center_height)+30),cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255, 255, 255), 2)
+
+    return cv2_im
+
+def drawing_line(cv2_im, center_widht, center_height):
+    cv2.line(cv2_im, (0, int(center_height)), (int(center_widht)*2, int(center_height)), (255, 255, 255))
+    cv2.line(cv2_im, (int(center_widht), 0), (int(center_widht), int(center_height)*2), (255, 255, 255))
+
+if __name__ == '__main__':
+    main()
+
+
